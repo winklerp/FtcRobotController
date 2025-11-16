28edb26b628a3b9505d1211304b8b2e947168e12
Philipp Winkler <philipp_winkler@outlook.com>
2025-11-16 20:57:02 +0100

    Added FTC-Dashboard and Panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpProgramPanels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpProgramPanels.java
@@ -1,36 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.robot.Robot;
+import static java.lang.Math.PI;
+import static java.lang.Math.sin;
 
-import org.firstinspires.ftc.teamcode.commands.MyCommand;
-import org.firstinspires.ftc.teamcode.commands.PositionsCommands;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.components.MyComponent;
-import org.firstinspires.ftc.teamcode.controlSystems.MyControlSystem;
-import org.firstinspires.ftc.teamcode.subSystemGroups.MySubsystemGroup;
 import org.firstinspires.ftc.teamcode.subSystems.Claw;
 import org.firstinspires.ftc.teamcode.subSystems.Lift;
 
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.CommandManager;
-import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
-import dev.nextftc.hardware.driving.FieldCentric;
-import dev.nextftc.hardware.driving.HolonomicMode;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
-import dev.nextftc.hardware.driving.RobotCentric;
 import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.IMUEx;
 import dev.nextftc.hardware.impl.MotorEx;
 
-@TeleOp(name = "NextFTC TeleOp Program Java", group = "Bot")
-public class TeleOpProgram extends NextFTCOpMode {
-    public TeleOpProgram() {
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.lights.Headlight;
+import com.bylazar.lights.PanelsLights;
+import com.bylazar.lights.RGBIndicator;
+import com.bylazar.lights.LightsManager;
+import com.bylazar.telemetry.TelemetryManager.TelemetryWrapper;
+
+@TeleOp(name = "NextFTC TeleOp Program Java Panels", group = "Bot")
+public class TeleOpProgramPanels extends NextFTCOpMode {
+    public TeleOpProgramPanels() {
         addComponents(
                 new SubsystemComponent(Lift.INSTANCE, Claw.INSTANCE),
                 BulkReadComponent.INSTANCE,
@@ -47,9 +50,34 @@ public class TeleOpProgram extends NextFTCOpMode {
     private final MotorEx backRightMotor = new MotorEx("back_right").brakeMode();
     private IMUEx imu = new IMUEx("imu", Direction.UP, Direction.FORWARD).zeroed();
 
-    @Override public void onInit() {
+    private final JoinedTelemetry joinedTelemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);
+    private final TelemetryWrapper panelTelemetry = PanelsTelemetry.INSTANCE.getFtcTelemetry();
 
+    private LightsManager lightManager = PanelsLights.INSTANCE.getLights();
+    private ElapsedTime timer = new ElapsedTime();
+    private RGBIndicator rgb;
+    private RGBIndicator rgb2;
+    private Headlight head;
+
+
+    @Override public void onInit() {
+        rgb = new RGBIndicator("Colored Light");
+        rgb.withProvider(() -> (sin(timer.seconds() * PI) + 1.0) / 2.0);
+
+        head = new Headlight("Simple Light");
+
+        rgb2 = new RGBIndicator("Colored Light #2");
+
+        rgb2.update(RGBIndicator.Companion.getBLUE());
+
+        lightManager.initLights(
+                rgb,
+                head,
+                rgb2
+        );
+        timer.reset();
     }
+
     @Override public void onWaitForStart() { }
     @Override
     public void onStartButtonPressed() {
@@ -103,7 +131,30 @@ public class TeleOpProgram extends NextFTCOpMode {
         //PositionsCommands.runToPosition(new MyControlSystem().controlSystem, 10).schedule();
     }
     @Override public void onUpdate() {
+        joinedTelemetry.addData("Key", "Value");
+        joinedTelemetry.addData("Key2", 50);
 
+        joinedTelemetry.update();
+
+        double t = timer.seconds();
+
+        boolean headOn = (((int)(t / 1.5)) % 2) == 0;
+
+        rgb.update(0.0);
+        head.update(headOn);
+
+        lightManager.update();
+
+        joinedTelemetry.addData("Time (s)", "%.2f", t);
+        lightManager.addToTelemetry(joinedTelemetry);
+        joinedTelemetry.update();
+
+        lightManager.getLightsState().forEach( (it) -> {
+            // panelTelemetry.addData("","${it.id} ${it.type} ${it.value}");
+            joinedTelemetry.addData("","${it.id} ${it.type} ${it.value}");
+        });
+
+        joinedTelemetry.update();
     }
 
     @Override public void onStop() { }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlSystems/MyControlSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlSystems/MyControlSystem.java
@@ -1,16 +1,22 @@
 package org.firstinspires.ftc.teamcode.controlSystems;
 
+import com.acmerobotics.dashboard.config.Config;
+
 import dev.nextftc.control.ControlSystem;
 import dev.nextftc.control.KineticState;
 import dev.nextftc.control.feedback.FeedbackType;
+import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.control.feedback.PIDElement;
 import dev.nextftc.control.feedforward.ArmFeedforward;
 import dev.nextftc.control.feedforward.GravityFeedforwardParameters;
 import dev.nextftc.control.interpolators.ConstantInterpolator;
 
+@Config
 public class MyControlSystem {
+    public static volatile PIDCoefficients coefficients = new PIDCoefficients(1, 0,0);
     public ControlSystem controlSystem = ControlSystem.builder()
-                .feedback(/* feedback element */ new PIDElement(FeedbackType.POSITION, 1, 0, 0))
+                //.feedback(/* feedback element */ new PIDElement(FeedbackType.POSITION, 1, 0, 0))
+                .posPid(coefficients)
                 .feedforward(/* feedforward element */ new ArmFeedforward(new GravityFeedforwardParameters(1, 2, 3,4)))
                 //.feedforward(new FullPowerFeedforward())
                 .posFilter(filter -> filter.custom(/* position filter */ new MyFilter() ))
